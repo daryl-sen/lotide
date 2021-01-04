3641e2acfd80ccc44a3715bebecfcaa029e753ab
Daryl <tsdaryl@gmail.com>
2021-01-04 21:48:37 +0000

    added head.js

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `👍️ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `❌️ Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const head = function(array) {
+  if (array !== []) {
+    return array[0];
+  }
+}
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+console.log(assertEqual(head([]), undefined));
