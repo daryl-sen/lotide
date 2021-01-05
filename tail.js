9bff33b85e9e449dd4aa1e184ccac46d7861c35b
daryl <tsdaryl@gmail.com>
2021-01-05 14:51:47 -0800

    fixed - swapped file contents between tail.js and eqArray.js

--- tail.js
+++ tail.js
@@ -1,26 +1,24 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
-    return false;
+    return `❌️ Assertion Failed: [${actual}] and [${expected}] have different lengths.`;
   } else {
     for (let i = 0; i < actual.length; i++) {
       if (actual[i] !== expected[i]) {
-        return false;
+        return `❌️ Assertion Failed: [${actual}] !== [${expected}]`;
       }
     }
-    return true;
+    return `👍️ Assertion Passed: [${actual}] === [${expected}]`;
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // TRUE
-assertEqual(eqArrays([1, 2, 3], [1, "2", 3]), true); // FALSE
-assertEqual(eqArrays([1, 2, 3], [1, "2", 3]), false); // TRUE
-assertEqual(eqArrays([1, 2, 3], [1, 2]), true); // FALSE
-assertEqual(eqArrays([1, 2, 3], []), true); // FALSE
+const tail = function(array) {
+  if (array !== []) {
+    const newArray = array.slice(1);
+    return newArray;
+  }
+};
+
+
+console.log(assertEqual(tail([4,5,6,7]), [5,6,7]));
+console.log(assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]));
+console.log(assertEqual(tail([]), []));
