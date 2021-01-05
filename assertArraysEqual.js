dba21b90e43919756635dd860a116ab900dfbe90
daryl <tsdaryl@gmail.com>
2021-01-05 15:33:23 -0800

    fixed assertArraysEqual, according to compass instructions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,21 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   if (actual.length !== expected.length) {
-    return false;
+    return console.log(`❌️ Assertion Failed: ${actual} !== ${expected}`);
   } else {
     for (let i = 0; i < actual.length; i++) {
       if (actual[i] !== expected[i]) {
-        return false;
+        return console.log(`❌️ Assertion Failed: ${actual} !== ${expected}`);
       }
     }
-    return true;
+    return console.log(`👍️ Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // TRUE
-assertEqual(eqArrays([1, 2, 3], [1, "2", 3]), true); // FALSE
-assertEqual(eqArrays([1, 2, 3], [1, "2", 3]), false); // TRUE
-assertEqual(eqArrays([1, 2, 3], [1, 2]), true); // FALSE
-assertEqual(eqArrays([1, 2, 3], []), true); // FALSE
+
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, "2", 3]);
+assertArraysEqual([1, 2, 3], [1, "2", 3]);
+assertArraysEqual([1, 2, 3], [1, 2]);
+assertArraysEqual([1, 2, 3], []);
---
+++ without.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const checkItem = function(target, array) {
+  for (let item of array) {
+    if (item === target) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const without = function(source, itemsToRemove) {
+  let output = [];
+  for (let item of source) {
+    if (checkItem(item, itemsToRemove)) {
+      output.push(item);
+    }
+  }
+  return output;
+};
+
+
+
+// test without function
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]));
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
+
+console.log(without([1, 2, 3], [1])); // output [2, 3]
+console.log(without([1, 2, 3, 4, 5], [1, 3, 5])); // output [2, 4]
+console.log(without([1, 2, "3", 4, 5], [1, 3, 5])); // output [2. "3", 5] because "3" is not the same as 3
+console.log(without([1, 2, 3, 4, 5], [1, "3", 5])); // output [2, 3, 4]
