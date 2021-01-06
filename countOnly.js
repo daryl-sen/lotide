81c983aa33aeaf811b256210ea7408ca83c26069
daryl <tsdaryl@gmail.com>
2021-01-06 13:23:23 -0800

    countOnly.js almost done, still showing 0s

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let output = {};
+  for (let entry in itemsToCount) {
+    output[entry] = 0;
+    for (let item of allItems) {
+      if (item === entry) {
+        output[entry]++;
+      }
+    }
+  }
+  // console.log(output);
+  return output;
+}
+
+
+countOnly(['a','a','b','b','b','c'], {a: true, b: true});
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
