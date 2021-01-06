ebbeeffb0202b75bac6f3b4d9782de3d32780dea
daryl <tsdaryl@gmail.com>
2021-01-05 16:40:00 -0800

    tested flatten.js and fixed linting errors

--- flatten.js
+++ flatten.js
@@ -29,32 +29,15 @@ const flatten = function(array) {
   let output = [];
   for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i])) {
-      console.log('array', array[i]);
+      for (let item of array[i]) {
+        output.push(item);
+      }
     } else {
-      console.log(array[i]);
+      output.push(array[i]);
     }
   }
-}
+  console.log(output);
+};
 
-
-// const flatten = function(array) {
-//   let output = [];
-//   for (let item of array) {
-//     if (Array.isArray(array)) {
-      
-//       console.log('array', item);
-//       // for (let subitem of item) {
-//       //   output.push(subitem);
-//       // }
-//     } else {
-//       console.log(item);
-//       // output.push(item);
-//     }
-//   }
-//   console.log(output);
-// };
-
-
-// console.log(Array.isArray([1,2,3][0]));
 
 flatten([1, 2, [3, 4], 5, [6]]);
